3eee5aa8dfa922a3918224a5f3ba800b861cfb0e
Christopher Morton <cmorton9249@users.noreply.github.com>
2020-10-01 12:28:20 -0400

    Initial commit

---
+++ IssueTracker/Models/Issue.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IssueTracker.Models
+{
+	public class Issue
+	{
+		public int Id { get; set; }
+		[Required]
+		[MaxLength(200)]
+		public string Description { get; set; }
+		[MaxLength(100000)]
+		public string Narrative { get; set; }
+		[Required]
+		public string AddedBy { get; set; }
+		[Required]
+		public string Status { get; set; }
+	}
+}
---
+++ IssueTracker/Models/IssuesDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IssueTracker.Models
+{
+	public class IssuesDataContext: DbContext
+	{
+		public IssuesDataContext(DbContextOptions<IssuesDataContext> options): base(options)
+		{
+
+		}
+
+		public virtual DbSet<Issue> Issues { get; set; }
+	}
+}
